1143c2c62ddd5eeb1f32ccc8fd03a91a40fc91db
terasdigital <terasdigital24@gmail.com>
2026-03-10 20:50:41 +0700

    first commit

---
+++ src/app/(auth)/login/_components/login.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import FormInput from "@/components/common/form-input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { INITIAL_LOGIN_FORM } from "@/constants/auth-constant";
+import { LoginForm, loginSchema } from "@/validations/auth-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+export default function Login() {
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: INITIAL_LOGIN_FORM,
+  });
+
+  const onSubmit = form.handleSubmit(async (data) => {});
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Welcome</CardTitle>
+        <CardDescription>Please sign in to continue</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <FormInput
+              form={form}
+              type="email"
+              name="email"
+              label="email"
+              placeholder="Insert email here"
+            />
+            <FormInput
+              form={form}
+              type="password"
+              name="password"
+              label="password"
+              placeholder="*****"
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field: { ...rest } }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...rest}
+                      type="password"
+                      placeholder="********"
+                      autoComplete="off"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "./_components/login";
+
+export const metadata = {
+  title: "Transco | Login",
+};
+
+export default function LoginPage() {
+  return <Login />;
+}
