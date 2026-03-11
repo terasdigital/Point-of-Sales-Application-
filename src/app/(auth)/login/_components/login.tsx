f19f3761c5a70f4de406b42af8b377e82ace46ad
terasdigital <terasdigital24@gmail.com>
2026-03-11 11:53:42 +0700

    feat: add toaster/sonner

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -21,6 +21,7 @@ import { useForm } from "react-hook-form";
 import { login } from "../actions";
 import { log } from "console";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
 
 export default function Login() {
   const form = useForm<LoginForm>({
@@ -45,6 +46,9 @@ export default function Login() {
 
   useEffect(() => {
     if (loginState.status === "error") {
+      toast.error("Login Failed", {
+        description: loginState.errors?._form?.[0],
+      });
       startTransition(() => {
         loginAction(null);
       });
