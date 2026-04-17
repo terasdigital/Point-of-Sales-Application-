0143b174535a3150d6daa1f49d57a146e4911cce
terasdigital <terasdigital24@gmail.com>
2026-04-17 11:25:22 +0700

    feat:setup input file image create user

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+  devIndicators: false,
+  images: {
+    domains: ["https://siyxjltxztzadthkhlpq.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "siyxjltxztzadthkhlpq.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/(dahboard)/admin/user/_components/dialog-create-user.tsx
+++ src/app/(dahboard)/admin/user/_components/dialog-create-user.tsx
@@ -20,11 +20,12 @@ import {
 } from "@/validations/auth-validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
-import { startTransition, useActionState, useEffect } from "react";
+import { startTransition, useActionState, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { createUser } from "../actions";
 import { toast } from "sonner";
 import FormSelect from "@/components/common/form-select";
+import FormImage from "@/components/common/form-image";
 
 export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const form = useForm<CreateUserForm>({
@@ -35,10 +36,17 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const [createUserState, createUserAction, isPendingCreateUser] =
     useActionState(createUser, INITIAL_STATE_CREATE_USER);
 
+  const [preview, setPreview] = useState<
+    { file: File; displayUrl: string } | undefined
+  >(undefined);
+
   const onSubmit = form.handleSubmit(async (data) => {
     const formData = new FormData();
     Object.entries(data).forEach(([key, value]) => {
-      formData.append(key, value);
+      formData.append(
+        key,
+        key === "avatar_url" ? (preview!.file ?? "") : value,
+      );
     });
     startTransition(() => {
       createUserAction(formData);
@@ -55,6 +63,7 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
     if (createUserState.status === "success") {
       toast.success("Create User Success");
       form.reset();
+      setPreview(undefined);
       document.querySelector<HTMLButtonElement>('[data-state="open"]')?.click();
       refetch();
     }
@@ -79,6 +88,13 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
             name="email"
             label="email"
             placeholder="Insert email here"
+          />
+          <FormImage
+            form={form}
+            name="avatar_url"
+            label="Avatar"
+            preview={preview}
+            setPreview={setPreview}
           />
           <FormSelect
             form={form}
