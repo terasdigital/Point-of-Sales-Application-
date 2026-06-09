08c637b28c11ded586f29a4d2288142d7f6cd406
terasdigital <terasdigital24@gmail.com>
2026-06-10 06:52:40 +0700

    feat:update status order item

--- src/app/(dahboard)/order/[id]/_components/detail-order.tsx
+++ src/app/(dahboard)/order/[id]/_components/detail-order.tsx
@@ -10,9 +10,18 @@ import { cn, convertIDR } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
-import { useMemo } from "react";
+import { startTransition, useActionState, useEffect, useMemo } from "react";
 import { toast } from "sonner";
 import Summary from "./summary";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVertical } from "lucide-react";
+import { updateStatusOrderItem } from "../../actions";
+import { INITIAL_STATE_ACTION } from "@/constants/general-constant";
 
 export default function DetailOrder({ id }: { id: string }) {
   const supabase = createClient();
@@ -41,7 +50,11 @@ export default function DetailOrder({ id }: { id: string }) {
     enabled: !!id,
   });
 
-  const { data: orderMenu, isLoading: isLoadingOrderMenu } = useQuery({
+  const {
+    data: orderMenu,
+    isLoading: isLoadingOrderMenu,
+    refetch: refetchOrderMenu,
+  } = useQuery({
     queryKey: ["orders_menu", order?.id, currentPage, currentLimit],
     queryFn: async () => {
       const result = await supabase
@@ -57,6 +70,37 @@ export default function DetailOrder({ id }: { id: string }) {
       return result;
     },
     enabled: !!order?.id,
+  });
+
+  const [updateStatusOrderState, updateStatusOrderAction] = useActionState(
+    updateStatusOrderItem,
+    INITIAL_STATE_ACTION,
+  );
+
+  const handleUpdateStatusOrder = async (data: {
+    id: string;
+    status: string;
+  }) => {
+    const formData = new FormData();
+    Object.entries(data).forEach(([Key, value]) => {
+      formData.append(Key, value);
+    });
+
+    startTransition(() => {
+      updateStatusOrderAction(formData);
+    });
+  };
+
+  useEffect(() => {
+    if (updateStatusOrderState?.status === "error") {
+      toast.error("Update Status Order Failed", {
+        description: updateStatusOrderState.errors?._form?.[0],
+      });
+    }
+    if (updateStatusOrderState?.status === "success") {
+      toast.success("Update Status Order Success");
+      refetchOrderMenu();
+    }
   });
 
   const filteredData = useMemo(() => {
@@ -84,12 +128,45 @@ export default function DetailOrder({ id }: { id: string }) {
             "bg-gray-500": item.status === "pending",
             "bg-yellow-500": item.status === "process",
             "bg-blue-500": item.status === "ready",
-            "bg-green-500": item.status === "serve",
+            "bg-green-500": item.status === "served",
           })}
         >
           {item.status}
         </div>,
-        <DropdownAction menu={[]} />,
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className={cn(
+                "data-[state=open]: bg-muted text-muted-foreground flex size-8",
+                { hidden: item.status === "served" },
+              )}
+              size="icon"
+            >
+              <EllipsisVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-40">
+            {["pending", "process", "ready"].map((status, index) => {
+              const nextStatus = ["process", "ready", "served"][index];
+              return (
+                item.status === status && (
+                  <DropdownMenuItem
+                    key={status}
+                    onClick={() =>
+                      handleUpdateStatusOrder({
+                        id: item.id,
+                        status: nextStatus,
+                      })
+                    }
+                  >
+                    {nextStatus}
+                  </DropdownMenuItem>
+                )
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>,
       ];
     });
   }, [orderMenu?.data]);
